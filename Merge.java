4c53415a8ec3710239f3cbc4f7cba6bf1a013c44
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-03-20 21:08:02 -0400

    Update Merge.java

--- Merge.java
+++ Merge.java
@@ -1,52 +1,79 @@
+
+import java.util.*;
+
 public class Merge{
+    public static void insertionSort(int[] ary, int lo, int hi){ 
+        for (int i = lo; i < hi; i++){ 
+            int cur = ary[i]; 
+            int j = i - 1; 
+            while(j >= lo && ary[j] > cur){ 
+                ary[j + 1] = ary[j]; 
+                j--;
+            } 
+            ary[j + 1] = cur; 
+        }
+    } 
 
-	public static void main(String[] args){
-		int[] stuff = new int[100];
-		for(int i = 0; i < 100; i++){
-			stuff[i] = (int)(100 * Math.random());
-		}
-		for(int x = 0; x < 100; x++){
-			System.out.print(stuff[x] + " ");
-		}
-		System.out.println();
-		mergesort(stuff);
-		for(int y = 0; y < 100; y++){
-			System.out.print(stuff[y] + " ");
-		}
-	}
-	/*sort the array from least to greatest value. This is a wrapper function*/
-	public static void mergesort(int[]data){
-		int[] temp = new int[data.length];
-		for(int i = 0; i < data.length; i++){
-			temp[i] = data[i];
-		}
-		mergesortH(data, temp, 0, data.length);
-	}
-
-	public static void mergesortH(int[] data, int[] temp, int lo, int hi){
-		if(lo >= hi) return;
-		mergesortH(temp, data, lo, (lo + hi)/2);
-		mergesortH(temp, data, (lo + hi)/2, hi);
-		//now merge them from temp to data, since that's how it was called
-		int a1 = lo;
-		int a2 = (lo + hi) / 2;
-		for(int i = lo; i < hi; i++){
-			if(a2 < hi && a1 < (lo + hi) / 2){
-				if(temp[a1] >= temp[a2]){
-					data[i] = temp[a1];
-					a1++;
-				}
-				if(temp[a1] < temp[a2]){
-					data[i] = temp[a2];
-					a2++;
-				}
-			}
-			if(a2 >= hi){
-				data[i] = temp[a1];
-			}
-			if(a1 >= (lo + hi) / 2){
-				data[i] = temp[a2];
-			}
-		}
-	}
-}//infinite recursion seems to be the problem
+    // Places the elements of the given data into sorted order
+    // using the merge sort algorithm.
+    // post: data is in sorted (nondecreasing) order
+    public static void mergesort(int[] data) {
+    	if(data.length <= 20){
+    		insertionSort(data, 0, data.length);
+    	}
+        if (data.length > 20) {
+            // split data into two halves
+            int[] left = leftHalf(data);
+            int[] right = rightHalf(data);
+            
+            // recursively sort the two halves
+            mergeSort(left);
+            mergeSort(right);
+            
+            // merge the sorted halves into a sorted whole
+            merge(data, left, right);
+        }
+    }
+    
+    // Returns the first half of the given data.
+    public static int[] leftHalf(int[] data) {
+        int size1 = data.length / 2;
+        int[] left = new int[size1];
+        for (int i = 0; i < size1; i++) {
+            left[i] = data[i];
+        }
+        return left;
+    }
+    
+    // Returns the second half of the given data.
+    public static int[] rightHalf(int[] data) {
+        int size1 = data.length / 2;
+        int size2 = data.length - size1;
+        int[] right = new int[size2];
+        for (int i = 0; i < size2; i++) {
+            right[i] = data[i + size1];
+        }
+        return right;
+    }
+    
+    // Merges the given left and right datas into the given 
+    // result data.  Second, working version.
+    // pre : result is empty; left/right are sorted
+    // post: result contains result of merging sorted lists;
+    public static void merge(int[] result, 
+                             int[] left, int[] right) {
+        int i1 = 0;   // index into left data
+        int i2 = 0;   // index into right data
+        
+        for (int i = 0; i < result.length; i++) {
+            if (i2 >= right.length || (i1 < left.length && 
+                    left[i1] <= right[i2])) {
+                result[i] = left[i1];    // take from left
+                i1++;
+            } else {
+                result[i] = right[i2];   // take from right
+                i2++;
+            }
+        }
+    }
+ }
